902e40832a50694f0687004a40ff834483896417
ytsuji27 <ytsuji91@gmail.com>
2019-04-29 16:31:26 -0700

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      flipped: false
+    }
+  }
+
+  flipCard = () => {
+    this.setState({ flipped: !this.state.flipped })
+  }
+
   render() {
+    const {name, stats, sprites} = this.props.pokemon
+
     return (
       <Card>
-        <div>
+        <div onClick={this.flipCard} >
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.flipped ? sprites.back : sprites.front} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {stats.find(o => o.name === "hp").value}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -7,6 +7,12 @@ class PokemonCollection extends React.Component {
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {this.props.pokemon.map(pokemon => {
+          return <PokemonCard
+                  pokemon={pokemon}
+                  key={pokemon.id}
+                 />
+        })}
       </Card.Group>
     )
   }
